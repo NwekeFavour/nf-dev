2d7d5599f3ade014c8952e11998ebdc9f852a011
NwekeFavour <buchijunior90@gmail.com>
2026-03-20 12:45:13 +0100

    update in portfolio

--- src/redux/projectSlice.js
+++ src/redux/projectSlice.js
@@ -1,47 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    projects: [
-        {
-        id: 1,
-        title: "Codex Technology Solutions",
-        tech: ["Next.js", "TailwindCSS", "redux", "shadcn/ui", "Cpanel(Hosting)"],
-        summary: "Developed a responsive agency website for Codex Technology Solutions, showcasing services and projects with a clean, modern interface.",
-        link: "https://www.codex.ng"
-        },
-        {
-            id: 2,
-            title: "Echomorrow",
-            tech: ["React.Js", "TailwindCSS", "Flowbite", "NodeJS", "Vercel(Frontend)", "Render(Backend)"],
-            summary: "A reflective web-app that allows users to write, schedule and send personal letters to themselves",
-            link: "https://echomorrow-beta.vercel.app"
-        },
-        {
-            id: 3,
-            title: "nacos unidel",
-            tech: ["vuejs", "bootstrap", "Vercel(Frontend)", "Backend(Render)"],
-            summary: "A web-app for the NACOS UNIDEL community",
-            link: "https://nacos-unidel.vercel.app"
-        },
-        {
-            id: 4,
-            title: "Sompu",
-            tech: ["Next.js", "TailwindCSS", "Shadcn", "Vercel"],
-            summary: "Created Sompu, a responsive link-in-bio site with a clean and user-friendly interface.",
-            link: "https://sompu.vercel.app"
-        }
-    ]
-}
+  projects: [
+    {
+      id: 1,
+      title: "Codex Technology Solutions",
+      tech: ["Next.js", "TailwindCSS", "redux", "shadcn/ui", "Cpanel(Hosting)"],
+      summary:
+        "Developed a responsive agency website for Codex Technology Solutions, showcasing services and projects with a clean, modern interface.",
+      link: "https://www.codex.ng",
+    },
+    {
+      id: 2,
+      title: "ZandMarket",
+      tech: ["React.js", "TailwindCSS", "Express.js", "PostgreSQL"],
+      summary:
+        "A grocery e-commerce store that allows users to browse, and purchase groceries online",
+      link: "https://zandmarket.co.uk",
+    },
+    {
+      id: 3,
+      title: "Layemart",
+      tech: ["React.js", "TailwindCSS", "Express.js", "MongoDB"],
+      summary:
+        "A multi-vendor marketplace platform connecting small and medium-sized businesses with customers",
+      link: "https://layemart.com",
+    },
+    {
+      id: 4,
+      title: "Sompu",
+      tech: ["Next.js", "TailwindCSS", "Shadcn", "Vercel"],
+      summary:
+        "Created Sompu, a responsive link-in-bio site with a clean and user-friendly interface.",
+      link: "https://sompu.vercel.app",
+    },
+  ],
+};
 
 const projectSlice = createSlice({
-    name: "projects",
-    initialState,
-    reducers: {
-        addProject: (state, action) => {
-            state.list.push(action.payload);
-        }
-    }
+  name: "projects",
+  initialState,
+  reducers: {
+    addProject: (state, action) => {
+      state.list.push(action.payload);
+    },
+  },
 });
 
-export const {addProject} = projectSlice.actions;
+export const { addProject } = projectSlice.actions;
 export default projectSlice.reducer;
